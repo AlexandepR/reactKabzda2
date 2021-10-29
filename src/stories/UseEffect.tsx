9b1cc38888549b8c10c92eb53ae01484261e9b34
Alex <alexandepp@gmail.com>
2021-10-29 19:17:56 +0300

    UseEffect 21

--- src/stories/UseEffect.tsx
+++ src/stories/UseEffect.tsx
@@ -5,19 +5,30 @@ export default {
 }
 
 export const SimpleExample = () => {
-    const [fake, setFake] = useState(1)
-    const [counter, setCounter] = useState(1)
+    const [sec, setSec] = useState(1)
+    const [minutes, setMinutes] = useState(0)
 
     useEffect(() => {
+
+        let time = new Date()
+
         setInterval (() => {
-            document.title = counter.toString()
+            console.log('tick')
+            // if ( sec <= 60 ) {
+            //     setSec((sate) => state + 1)
+            // }
+            // else if (sec >= 61) {
+            //     setSec(0)
+            // }
+           // sec <= 60 ? setSec((state) => state + 1) : setMinutes((state) =>state + 1) ?
+           //  sec >= 61 : setSec(0)
         }, 1000)
-    })
+
+    },[sec])
 
     return <>
-    Hello, {counter} {fake}
-        <button onClick={() => setFake(fake + 1)}>+Fake</button>
-        <button onClick={() => setCounter(counter + 1)}>+Counter</button>
+    Hello, sec: {sec} - minutes: {minutes}
+
     </>
 }
 
