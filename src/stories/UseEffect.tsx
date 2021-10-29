b2366689caf97191005aa0b165531cd8f94b5fee
Alex <alexandepp@gmail.com>
2021-10-29 17:10:32 +0300

    UseEffect

--- src/stories/UseEffect.tsx
+++ src/stories/UseEffect.tsx
@@ -7,22 +7,12 @@ export default {
 export const SimpleExample = () => {
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
-    console.log('SimpleExample');
 
     useEffect(() => {
-        console.log('useEffect every render');
-        document.title = counter.toString()
+        setInterval (() => {
+            document.title = counter.toString()
+        }, 1000)
     })
-
-    useEffect(() => {
-        console.log('useEffect only first render(componentDidMount)');
-        document.title = counter.toString()
-    }, [])
-
-    useEffect(() => {
-        console.log('useEffect first render and every counter change change');
-        document.title = counter.toString()
-    },[counter])
 
     return <>
     Hello, {counter} {fake}
@@ -30,3 +20,42 @@ export const SimpleExample = () => {
         <button onClick={() => setCounter(counter + 1)}>+Counter</button>
     </>
 }
+
+
+
+
+
+
+
+
+
+// export default {
+//     title: 'useEffect demo'
+// }
+//
+// export const SimpleExample = () => {
+//     const [fake, setFake] = useState(1)
+//     const [counter, setCounter] = useState(1)
+//     console.log('SimpleExample');
+//
+//     useEffect(() => {
+//         console.log('useEffect every render');
+//         document.title = counter.toString()
+//     })
+//
+//     useEffect(() => {
+//         console.log('useEffect only first render(componentDidMount)');
+//         document.title = counter.toString()
+//     }, [])
+//
+//     useEffect(() => {
+//         console.log('useEffect first render and every counter change');
+//         document.title = counter.toString()
+//     },[counter])
+//
+//     return <>
+//         Hello, {counter} {fake}
+//         <button onClick={() => setFake(fake + 1)}>+Fake</button>
+//         <button onClick={() => setCounter(counter + 1)}>+Counter</button>
+//     </>
+// }
